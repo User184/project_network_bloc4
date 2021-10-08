ba41847aa82f000bffa6afa4c50d37fddcf6d920
Юрий Крюченко <yurii.kryuchenko@webpp.ru>
2021-10-08 11:37:23 +0300

    Доработал SimpleBlocObserver

--- lib/bloc/simple_bloc_observer.dart
+++ lib/bloc/simple_bloc_observer.dart
@@ -2,20 +2,38 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SimpleBlocObserver extends BlocObserver {
   @override
+  void onCreate(BlocBase bloc) {
+    super.onCreate(bloc);
+    print('Create: ${bloc.runtimeType}');
+  }
+
+  @override
   void onEvent(Bloc bloc, Object? event) {
     super.onEvent(bloc, event);
-    print(event);
+    print('Event: $event');
+  }
+
+  @override
+  void onChange(BlocBase bloc, Change change) {
+    super.onChange(bloc, change);
+    print('Change: $change');
   }
 
   @override
   void onTransition(Bloc bloc, Transition transition) {
     super.onTransition(bloc, transition);
-    print(transition);
+    print('Transition: $transition');
   }
 
   @override
   void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
-    print(error);
+    print('Error: $error');
     super.onError(bloc, error, stackTrace);
+  }
+
+  @override
+  void onClose(BlocBase bloc) {
+    super.onClose(bloc);
+    print('Close: ${bloc.runtimeType}');
   }
 }
